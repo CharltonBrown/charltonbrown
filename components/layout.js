bd323e596b14f0004cc716a42e5a77a9b313401e
Mikey Holland <mpjholland@gmail.com>
2023-07-05 18:56:36 +0100

    Create layout component

---
+++ components/layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Layout({ children, title }) {
+  return (
+    <>
+      <Head>
+        <title>{title} - Charlton Brown</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      {children}
+    </>
+  );
+}
