077651ebf64e70142cf002cb3087c8201a7dcd67
Mikey Holland <mpjholland@gmail.com>
2023-07-14 15:57:29 +0100

    Add initial layout to people page

--- pages/about-us/people.js
+++ pages/about-us/people.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useQuerySubscription } from 'react-datocms';
+import Image from 'next/image';
 
 import request from '@/lib/datocms';
 import Layout from '@/components/layout';
+import { responsiveImageFragment } from '@/lib/fragments';
 import mainNavigationFragment from '@/components/navigation/fragment';
 import footerNavigationFragment from '@/components/footerNavigation/fragment';
 
@@ -18,10 +20,16 @@ export async function getStaticProps({ preview = false }) {
               name
               phone
               position
+              image {
+                responsiveImage(imgixParams: {fm: jpg, w: 500 }) {
+                  ...responsiveImageFragment
+                }
+              }
             }
             ${mainNavigationFragment}
             ${footerNavigationFragment}
           }
+          ${responsiveImageFragment}
         `,
     preview,
   };
@@ -54,7 +62,54 @@ export default function People({ subscription }) {
       mainNavigation={mainNavigation.links}
       footerNavigation={footerNavigation.links}
     >
-      <div className="p-8">people</div>
+      <h1 className="sr-only">People</h1>
+
+      <div className="p-8">
+        <div className="flex">
+          <aside className="w-[220px] h-screen sticky top-0 flex flex-col justify-center">
+            <nav>
+              <ul className="flex flex-col">
+                <li>
+                  <button type="button" className="text-xl text-silver ">
+                    About us
+                  </button>
+                </li>
+                <li>
+                  <button type="button" className="text-xl text-silver ">
+                    People
+                  </button>
+                </li>
+                <li>
+                  <button type="button" className="text-xl text-silver ">
+                    Journal
+                  </button>
+                </li>
+                <li>
+                  <button type="button" className="text-xl text-silver ">
+                    Careers
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          </aside>
+          <div className="pt-[200px] pl-8 grid grid-cols-4 gap-12">
+            {people.map((person) => (
+              <article key={person.id} className="mb-4">
+                <Image
+                  className="object-cover mb-4"
+                  width={person.image.responsiveImage.width}
+                  height={person.image.responsiveImage.height}
+                  src={person.image.responsiveImage.src}
+                  alt={person.image.responsiveImage.alt}
+                />
+                <div className="flex flex-col">
+                  <h2 className="text-2xl">{person.name}</h2>
+                </div>
+              </article>
+            ))}
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
