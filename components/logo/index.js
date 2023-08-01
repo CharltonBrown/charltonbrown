7b603f731022907d44a075895ea3dd05c95e62d4
Mikey Holland <mpjholland@gmail.com>
2023-08-02 00:04:58 +0100

    Add motif footnote to Logo

--- components/logo/index.js
+++ components/logo/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
 
+import FootnoteThree from '@/components/icons/footnotes/footnote-3';
+
 export default function Logo() {
   return (
-    <Link href="/" className="flex flex-col text-2xl">
+    <Link href="/" className="relative flex flex-col pl-3 text-2xl">
+      <FootnoteThree className="absolute top-0 left-0 w-2 h-2 stroke-white" />
       <span>Charlton Brown</span>
       <span>Architecture & Interiors</span>
     </Link>
