bd90179677876a78b6ff635da4dc790d9ce50487
Mikey Holland <mpjholland@gmail.com>
2023-08-24 15:07:00 +0100

    Add FadeInBlock to homepage Footnote svg

--- pages/index.js
+++ pages/index.js
@@ -95,68 +95,78 @@ export default function Home({ subscription }) {
         <div className="relative z-50">
           {/* Empty div for fixed hero */}
           <div className="w-full h-screen" ref={heroRef} />
-          <div className="relative h-screen flex items-center justify-center py-8 bg-bone">
-            <Container>
-              <FadeInBlock>
-                <div className="max-w-3xl mx-auto py-8 px-5 md:px-7 lg:px-10">
-                  <h2 className="text-3xl lg:text-5xl">{introLabel}</h2>
-                  <div
-                    className="text-3xl lg:text-5xl font-savoyItalic"
-                    dangerouslySetInnerHTML={{
-                      __html: quote,
-                    }}
-                  />
+          <div className="bg-bone">
+            <div className="relative h-screen flex items-center justify-center py-8">
+              <Container>
+                <FadeInBlock>
+                  <div className="max-w-3xl mx-auto py-8 px-5 md:px-7 lg:px-10">
+                    <h2 className="text-3xl lg:text-5xl">{introLabel}</h2>
+                    <div
+                      className="text-3xl lg:text-5xl font-savoyItalic"
+                      dangerouslySetInnerHTML={{
+                        __html: quote,
+                      }}
+                    />
+                  </div>
+                  <div className="max-w-3xl mx-auto py-8 px-5 md:px-7 lg:px-10">
+                    <h2 className="uppercase text-xl lg:text-2xl mb-4 font-savoyBold tracking-wider">
+                      {blockOneLabel}
+                    </h2>
+                    <div
+                      className="text-xl lg:text-2xl"
+                      dangerouslySetInnerHTML={{
+                        __html: blockOneBody,
+                      }}
+                    />
+                  </div>
+                </FadeInBlock>
+                <div className="absolute bottom-0 w-full inset-x-0">
+                  <FadeInBlock>
+                    <FootnoteOne className="absolute left-1/2 -translate-y-1/2 bottom-16 w-4" />
+                  </FadeInBlock>
                 </div>
-                <div className="max-w-3xl mx-auto py-8 px-5 md:px-7 lg:px-10">
-                  <h2 className="uppercase text-xl lg:text-2xl mb-4 font-savoyBold tracking-wider">
-                    {blockOneLabel}
-                  </h2>
-                  <div
-                    className="text-xl lg:text-2xl"
-                    dangerouslySetInnerHTML={{
-                      __html: blockOneBody,
-                    }}
-                  />
+              </Container>
+            </div>
+            <div className="relative h-screen flex items-center justify-center">
+              <Container>
+                <FadeInBlock>
+                  <div className="max-w-3xl mx-auto py-8 px-5 md:px-7 lg:px-10">
+                    <h2 className="uppercase text-xl lg:text-2xl mb-4 font-savoyBold tracking-wider">
+                      {blockTwoLabel}
+                    </h2>
+                    <div
+                      className="text-xl lg:text-2xl"
+                      dangerouslySetInnerHTML={{
+                        __html: blockTwoBody,
+                      }}
+                    />
+                  </div>
+                </FadeInBlock>
+                <div className="absolute bottom-0 w-full inset-x-0">
+                  <FadeInBlock>
+                    <FootnoteOne className="absolute left-1/2 -translate-y-1/2 bottom-16 w-4" />
+                  </FadeInBlock>
                 </div>
-              </FadeInBlock>
-            </Container>
-            <FootnoteOne className="absolute left-1/2 -translate-y-1/2 bottom-16 w-4" />
-          </div>
-          <div className="relative h-screen flex items-center justify-center bg-bone">
-            <Container>
-              <FadeInBlock>
-                <div className="max-w-3xl mx-auto py-8 px-5 md:px-7 lg:px-10">
-                  <h2 className="uppercase text-xl lg:text-2xl mb-4 font-savoyBold tracking-wider">
-                    {blockTwoLabel}
-                  </h2>
-                  <div
-                    className="text-xl lg:text-2xl"
-                    dangerouslySetInnerHTML={{
-                      __html: blockTwoBody,
-                    }}
-                  />
-                </div>
-              </FadeInBlock>
-              <FootnoteOne className="absolute left-1/2 -translate-y-1/2 bottom-16 w-4" />
-            </Container>
-          </div>
-          <div className="h-screen flex items-center justify-center bg-bone">
-            <Container>
-              <FadeInBlock>
-                <div className="max-w-3xl mx-auto py-8 px-5 md:px-7 lg:px-10">
-                  <h2 className="mx-auto uppercase text-xl lg:text-2xl mb-4 font-savoyBold tracking-wider">
-                    {blockThreeLabel}
-                  </h2>
-                  <div
-                    className="mx-auto text-xl lg:text-2xl"
-                    dangerouslySetInnerHTML={{
-                      __html: blockThreeBody,
-                    }}
-                  />
-                  <MotifNavigation links={mainNavigation.links} />
-                </div>
-              </FadeInBlock>
-            </Container>
+              </Container>
+            </div>
+            <div className="h-screen flex items-center justify-center">
+              <Container>
+                <FadeInBlock>
+                  <div className="max-w-3xl mx-auto py-8 px-5 md:px-7 lg:px-10">
+                    <h2 className="mx-auto uppercase text-xl lg:text-2xl mb-4 font-savoyBold tracking-wider">
+                      {blockThreeLabel}
+                    </h2>
+                    <div
+                      className="mx-auto text-xl lg:text-2xl"
+                      dangerouslySetInnerHTML={{
+                        __html: blockThreeBody,
+                      }}
+                    />
+                    <MotifNavigation links={mainNavigation.links} />
+                  </div>
+                </FadeInBlock>
+              </Container>
+            </div>
           </div>
         </div>
       </main>
