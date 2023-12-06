0e89edae059e1c4284486d50e5f5e76dba9cd065
Mikey Holland <mpjholland@gmail.com>
2023-12-06 15:46:22 +0000

    Update ScrollPrompt to use useAnimate

--- components/scroll-prompt/index.js
+++ components/scroll-prompt/index.js
@@ -1,64 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import clsx from 'clsx';
-import { motion, cubicBezier } from 'framer-motion';
-
-const parentVariants = {
-  visible: {
-    transition: {
-      staggerChildren: 0.25,
-    },
-  },
-  hidden: {
-    transition: {
-      staggerChildren: 0.25,
-      staggerDirection: -1,
-    },
-  },
-};
-
-const opacityVariants = {
-  visible: {
-    opacity: 1,
-    transition: {
-      duration: 0.25,
-    },
-  },
-  hidden: {
-    opacity: 0,
-    transition: {
-      duration: 0.25,
-    },
-  },
-};
-
-const heightVariants = {
-  visible: {
-    height: 120,
-    transition: {
-      duration: 0.5,
-    },
-  },
-  hidden: {
-    height: 0,
-    transition: {
-      duration: 0.25,
-    },
-  },
-};
+import { cubicBezier, useAnimate } from 'framer-motion';
 
 export default function ScrollPrompt({ className }) {
-  const [visibility, setVisibility] = useState('hidden');
   const [scrollTop, setScrollTop] = useState(0);
+  const [textScope, animateText] = useAnimate();
+  const [lineScope, animateLine] = useAnimate();
+
+  const show = useCallback(async () => {
+    await animateText(textScope.current, { opacity: 1 }, { duration: 0.25 });
+    await animateLine(lineScope.current, { height: 120 }, { duration: 0.25 });
+    await animateLine(
+      lineScope.current,
+      { y: 10 },
+      {
+        duration: 0.75,
+        repeat: Infinity,
+        repeatType: 'reverse',
+        ease: cubicBezier(0.0, 0.0, 0.2, 1),
+      },
+    );
+  }, [animateLine, animateText, lineScope, textScope]);
+
+  const hide = useCallback(async () => {
+    await animateLine(lineScope.current, { height: 0 }, { duration: 0.25 });
+    await animateLine(lineScope.current, { y: 0 });
+    await animateText(textScope.current, { opacity: 0 }, { duration: 0.25 });
+  }, [animateLine, animateText, lineScope, textScope]);
 
   useEffect(() => {
     if (scrollTop === 0) {
       setTimeout(() => {
-        setVisibility('visible');
+        show();
       }, 2000);
     } else {
-      setVisibility('hidden');
+      hide();
     }
-  }, [scrollTop]);
+  }, [hide, scrollTop, show]);
 
   useEffect(() => {
     const onScroll = (e) => {
@@ -71,30 +49,15 @@ export default function ScrollPrompt({ className }) {
 
   return (
     <div className={clsx('overflow-hidden', className)}>
-      <motion.div
-        animate={visibility}
-        variants={parentVariants}
-        initial="hidden"
-        className="flex flex-col h-36 items-center text-white"
-      >
-        <motion.p variants={opacityVariants} className="text-lg mb-2">
+      <div className="flex flex-col h-36 items-center text-white">
+        <p ref={textScope} className="text-lg mb-2 opacity-0">
           Scroll
-        </motion.p>
-        <motion.div variants={heightVariants} className="overflow-hidden">
-          <motion.div
-            animate={{
-              y: 10,
-            }}
-            transition={{
-              duration: 0.75,
-              repeat: Infinity,
-              repeatType: 'reverse',
-              ease: cubicBezier(0.0, 0.0, 0.2, 1),
-            }}
-            className="w-px h-32 bg-white will-change-transform"
-          />
-        </motion.div>
-      </motion.div>
+        </p>
+        <div
+          ref={lineScope}
+          className="w-px h-0 bg-white will-change-transform"
+        />
+      </div>
     </div>
   );
 }
